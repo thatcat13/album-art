0d4092e78d8a6c107478240c808d83d711ca7f8c
Cat Janowitz and Sam Kissee <cat@>
2017-12-14 13:50:23 -0800

    add more  animations

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,6 @@ function AlbumItem(band, title, release, img){
 
 
 $(document).ready(function(){
-  $('#rating').barrating({
-    theme: 'bars-movie'
-    });
   $(".album-submit").submit(function(event) {
     event.preventDefault();
     var bandInput = $("input#band-input").val();
@@ -21,14 +18,31 @@ $(document).ready(function(){
 
     var newAlbum = new AlbumItem(bandInput, albumInput, releaseInput, imgInput);
     $(".gallery").prepend(
-      `<div class="album-item">
-        <img class="album-img" src="`+ newAlbum.img +`" alt="">
-        <p class="band">` + newAlbum. band + `</p>
-        <p class="album">`+ newAlbum.title +`</p>
-        <p class="release-date">`+ newAlbum.release +`</p>
+      `<div class="gallery-object">
+        <div class="album-item">
+          <img class="album-img" src="`+ newAlbum.img +`" alt="">
+          <div class="album-content">
+            <p class="band"><span class="title">Band: </span> ` + newAlbum. band + `</p>
+            <p class="album"><span class="title">Album: </span> `+ newAlbum.title +`</p>
+            <p class="release-date"><span class="title">Released: </span> `+ newAlbum.release +`</p>
+            <div class="rating-area">
+              <p>Rate Me!</p>
+              <select id="rating">
+                <option value="1">Bad</option>
+                <option value="2">Meh</option>
+                <option value="3">Not Horrible</option>
+                <option value="4">Pretty Alright</option>
+                <option value="5">AWESOME!</option>
+              </select>
+            </div>
+          </div>
+        </div>
       </div>`
     );
     // $(".album-submit").trigger("reset");
+    $('#rating').barrating({
+      theme: 'bars-movie'
+    });
     // $(".album-submit").empty();
     $(".album-submit")[0].reset();
   });
